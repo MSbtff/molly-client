a9ea51d78b735afd984dfd14bd71c6fbfa0aeaf8
MS_btff <whdghks845@gmail.com>
2025-03-10 09:46:51 +0900

    fix:jest setup 변경

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,3 @@
-// /** @type {import('ts-jest').JestConfigWithTsJest} **/
-
-// // import type { Config } from 'jest';
-// // import '@testing-library/jest-dom';
-
-// // const config: Config = {
-// //   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], // ✅ Jest 실행 전에 setup 파일 로드
-// //   preset: 'ts-jest',
-// //   testEnvironment: 'node',
-// // };
-
-// // export default config;
-
-
-// module.exports = {
-//     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-//     preset: 'ts-jest',
-//     testEnvironment: "node",
-//     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'], 
-//   };
-
 import type {Config} from 'jest';
 import nextJest from 'next/jest.js';
 
@@ -29,11 +8,6 @@ const createJestConfig = nextJest({
 
 const config: Config = {
   clearMocks: true,
-<<<<<<< HEAD
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
-=======
 
   collectCoverage: true,
 
@@ -41,7 +15,6 @@ const config: Config = {
 
   coverageProvider: 'v8',
 
->>>>>>> 8a24f3a5f730f0037ef30cff7fd0b04a3ca0e3e5
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
@@ -50,18 +23,7 @@ const config: Config = {
   },
 
   testEnvironment: 'jsdom',
-<<<<<<< HEAD
-=======
-
->>>>>>> 8a24f3a5f730f0037ef30cff7fd0b04a3ca0e3e5
-  // The glob patterns Jest uses to detect test files
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
 };
 
-<<<<<<< HEAD
 export default createJestConfig(config);
-
-
-=======
-export default createJestConfig(config);
->>>>>>> 8a24f3a5f730f0037ef30cff7fd0b04a3ca0e3e5
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,1 @@
 import '@testing-library/jest-dom';
-<<<<<<< HEAD
-
-=======
->>>>>>> 8a24f3a5f730f0037ef30cff7fd0b04a3ca0e3e5
