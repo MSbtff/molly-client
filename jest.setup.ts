beabad1a228c764d2852a8b9b24168a8d94acabf
MS_btff <whdghks845@gmail.com>
2025-03-10 09:03:57 +0900

    test: fetchAPI 테스트 작성

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,14 +1,2 @@
 import '@testing-library/jest-dom';
 
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      toBeInTheDocument(): R;
-      toHaveTextContent(text: string | RegExp): R;
-      toBeVisible(): R;
-      // 필요한 다른 matcher들 추가
-    }
-  }
-}
-
-export {};
