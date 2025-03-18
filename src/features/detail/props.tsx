7913633c56a03ac4c689e6fe687f8e51677753f6
Seeun <156633780+CuteSeeun@users.noreply.github.com>
2025-03-18 16:52:10 +0900

    fix: 빌드 에러 해결

--- src/features/detail/props.tsx
+++ src/features/detail/props.tsx
@@ -1,21 +1,21 @@
-import ProductDetail from "../../../app/detail/[productId]/page";
-import { fetchReviews } from "@/features/detail/api/action"; // 서버 액션 가져오기
+// import ProductDetail from "../../../app/detail/[productId]/page";
+// import { fetchReviews } from "@/features/detail/api/action"; // 서버 액션 가져오기
 
-export default async function ProductDetailPage({
-  params,
-}: {
-  params: { productId: string };
-}) {
-  const productId = Number(params.productId);
+// export default async function ProductDetailPage({
+//   params,
+// }: {
+//   params: { productId: string };
+// }) {
+//   const productId = Number(params.productId);
 
-  // 서버에서 리뷰 데이터를 가져옴
-  const reviewData = await fetchReviews(productId);
+//   // 서버에서 리뷰 데이터를 가져옴
+//   const reviewData = await fetchReviews(productId);
 
-  if (!reviewData) {
-    return <p>리뷰 데이터를 불러오는 중 오류가 발생했습니다.</p>; // 예외 처리 추가
-  }
+//   if (!reviewData) {
+//     return <p>리뷰 데이터를 불러오는 중 오류가 발생했습니다.</p>; // 예외 처리 추가
+//   }
 
-  return (
-    <ProductDetail productId={productId} initialReviews={reviewData.reviews} />
-  );
-}
+//   return (
+//     <ProductDetail productId={productId} initialReviews={reviewData.reviews} />
+//   );
+// }
