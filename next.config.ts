4072277e5d1b2f825b16dd4c613b088f17fec12f
MS_btff <whdghks845@gmail.com>
2025-03-18 17:49:22 +0900

    fix: 빌드 에러 수정

--- next.config.ts
+++ next.config.ts
@@ -15,7 +15,6 @@ const nextConfig: NextConfig = {
 
   images: {
     formats: ["image/webp"],
-    unoptimized: true,
     domains: ["3.35.175.203", "172.16.24.53", "persimmontree.ddns.net"], // API에서 제공하는 이미지 도메인(IP) (외부 이미지 도메인 허용)
     remotePatterns: [
       {
